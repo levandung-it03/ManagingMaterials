af676552e293d55cc18016adb8acbdd0b92c9df0
vgbao1231 <vgbao1231@gmail.com>
2024-05-27 20:18:28 +0700

    Fix bug close dialog by vogiabao

--- src/main/resources/static/js/base.js
+++ src/main/resources/static/js/base.js
@@ -463,11 +463,15 @@ async function CustomizeToggleOpeningAddingFormDialogSupporter(
 
     //--Customize closing dialog action.
     //--Stop propagation when clicking on dialog container so when select from list, it won't close the dialog
-    $('div.select-dialog-container').addEventListener("click", e => e.stopPropagation());
+    $$('div.select-dialog-container').forEach(dialog => {
+        dialog.addEventListener("click", e => e.stopPropagation())
+    })
     //--Close dialog when clicking on dialog
-    selectDialog.addEventListener("click", e => selectDialog.classList.add("closed"));
+    selectDialog.addEventListener("click", () => selectDialog.classList.add("closed"));
     //--Close dialog when clicking on close button
-    $('div.closing-dialog-btn').addEventListener("click", e => selectDialog.classList.add("closed"));
+    $$('div.closing-dialog-btn').forEach(btn => {
+        btn.addEventListener("click", () => selectDialog.classList.add("closed"));
+    })
 
     await CustomizeFetchingActionSpectator(
         searchingSupportingDataSource,
